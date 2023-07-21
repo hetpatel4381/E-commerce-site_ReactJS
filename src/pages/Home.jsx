a50a96a2019fe6fad81a7663534c3cfcb2a12002
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-07-21 15:48:10 +0530

    completed the banner section of Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -6,8 +7,12 @@ const Home = () => {
       <section className='banner'>
         <div className="container-xxl">
           <div className="row">
-            <div className="banner-details">
-
+            <div className="banner-details p-5 d-flex flex-column align-items-start justify-content-center">
+              <span className='shadow-lg p-3 text-white'>Trade in Offer!</span>
+              <h1>Super Value Deals</h1>
+              <h2>On all Products</h2>
+              <p>Save more with Apneck</p>
+              <Link className='button-link mb-3'>Shop Now</Link>
             </div>
           </div>
         </div>
