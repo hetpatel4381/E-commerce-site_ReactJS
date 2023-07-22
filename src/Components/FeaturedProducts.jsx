77f671f4a4bde2adb3f1ea418a3e83a3fbfbee96
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-07-22 14:38:40 +0530

    completed the repair section on Home page

--- src/Components/FeaturedProducts.jsx
+++ src/Components/FeaturedProducts.jsx
@@ -15,13 +15,15 @@ const FeaturedProducts = () => {
                   className="card-img-top img-fluid m-auto p-3"
                 />
                 <div className="card-details">
-                  <div className="title">
+                  <div className="title mb-3">
                     <span>{product.brand}</span>
-                    <h5>{product.name}</h5>
+                    <h5 className='mt-2'>{product.name}</h5>
                   </div>
                   <div className="card-footer text-center mb-3">
-                    <p>{product.status}</p>
-                    <p>{product.rate}</p>
+                    <p className="mb-3">{product.status}</p>
+                    <p>
+                      <strike className='text-danger'>{product.rate}</strike>
+                    </p>
                   </div>
                 </div>
               </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,6 +35,26 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      <section className="repair">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="repair-details text-center d-flex flex-column align-items-center p-3">
+              <span className="text-white mt-1">Repair Services</span>
+              <h2 className="text-white">
+                On{" "}
+                <span className="text-success">
+                  <b>70%</b>
+                </span>{" "}
+                Off on All Products And Accessories
+              </h2>
+              <div className="mt-3">
+                <button className='button-repair'>Explore Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
